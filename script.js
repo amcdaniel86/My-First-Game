27b8f17cd5d3a8382356b0b411a2961ab16481b3
Alex McDaniel <amcdaniel186@gmail.com>
2018-11-05 14:26:09 -0500

    update1

---
+++ script.js
@@ -0,0 +1,11 @@
+class Pong-Destroyer{
+      constructor()
+}
+
+
+// classes,
+// player 1 and 2
+// properties and display (canvas) static image background
+// hit 2 scores in arow, 3 etc, player 1 is faster, pl 2 slower.
+// pong or memory game.
+// bunch of if statements to cause changes when certain events happen. like colors changing, points change, effects change.
